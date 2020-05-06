8a402dbfe899821d83d7d778e4fbc0545c072a38
Meryem Elibal <merycicek@gmail.com>
2020-05-06 21:10:54 +0000

    added user data

--- asg.tf
+++ asg.tf
@@ -4,6 +4,8 @@ resource "aws_launch_configuration" "as_conf" {
   image_id = "${data.aws_ami.image.id}"
 
   instance_type = "t2.micro"
+
+  user_data = "${file("wordpress.sh")}"
 }
 
 resource "aws_autoscaling_group" "bar" {
