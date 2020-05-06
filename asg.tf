bbfb17f24bf22677d66d3109a2cc8de05fdc452b
Meryem Elibal <merycicek@gmail.com>
2020-05-06 21:48:15 +0000

    added new mix instances

--- asg.tf
+++ asg.tf
@@ -1,30 +1,41 @@
-resource "aws_launch_configuration" "as_conf" {
-  name = "web_config"
+resource "aws_launch_template" "example" {
+  name_prefix = "example"
 
   image_id = "${data.aws_ami.image.id}"
 
-  instance_type = "t2.micro"
-
-  user_data = "${file("wordpress.sh")}"
-  spot_price = "0.1"
+  instance_type = "c5.large"
 }
 
-resource "aws_autoscaling_group" "bar" {
-  name = "terraform-asg-example"
-
-  launch_configuration = "${aws_launch_configuration.as_conf.name}"
-
+resource "aws_autoscaling_group" "example" {
   availability_zones = [
     "us-east-1a",
     "us-east-1b",
     "us-east-1c",
   ]
 
+  desired_capacity = 1
+
+  max_size = 1
+
   min_size = 1
 
-  max_size = 2
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = "${aws_launch_template.example.id}"
+      }
 
-  lifecycle {
-    create_before_destroy = true
+      override {
+        instance_type = "c4.large"
+
+        weighted_capacity = "3"
+      }
+
+      override {
+        instance_type = "c3.large"
+
+        weighted_capacity = "2"
+      }
+    }
   }
 }
