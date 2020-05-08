cb25e53e8fb81cafb7c79f37a8f370c5de9f4cad
Meryem Elibal <merycicek@gmail.com>
2020-05-08 13:27:27 +0000

    added new changes

--- app/asg_module.tf
+++ app/asg_module.tf
@@ -13,20 +13,48 @@ module "app1" {
 module "wordpress-london" {
   source = "./module"
   region = "eu-west-2"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
 }
 
 module "oregon" {
   source = "./module"
 
   region = "us-west-2"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
 }
 
 module "Ohio" {
   source = "./module"
   region = "us-east-2"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
 }
 
 module "ireland" {
   source = "./module"
   region = "eu-west-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
 }
