eb536fedf1ad3146497bd8ad2a280083628bda01
Meryem Elibal <merycicek@gmail.com>
2020-05-08 02:55:31 +0000

    added interpolation for region

--- app/module/asg.tf
+++ app/module/asg.tf
@@ -8,9 +8,9 @@ resource "aws_launch_template" "example" {
 
 resource "aws_autoscaling_group" "example" {
   availability_zones = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
+    "${var.region}a",
+    "${var.region}b",
+    "${var.region}c",
   ]
 
   desired_capacity = 1
