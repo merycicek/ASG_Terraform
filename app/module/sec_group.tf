61dd1fc59b7e7f0b11106fa8d9d3ce1cd6d43c37
Meryem Elibal <merycicek@gmail.com>
2020-05-08 12:27:58 +0000

    add port 80 and 443

--- app/module/sec_group.tf
+++ app/module/sec_group.tf
@@ -15,6 +15,30 @@ resource "aws_security_group" "asg-sec-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    description = "TLS from VPC"
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description = "TLS from VPC"
+
+    from_port = 443
+
+    to_port = 443
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
 
