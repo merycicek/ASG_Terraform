59ec56f16d0ce2934def03c22f9054d9a57f84eb
Meryem Elibal <merycicek@gmail.com>
2020-05-08 13:45:27 +0000

    added regions

--- app/asg_module.tf
+++ app/asg_module.tf
@@ -35,6 +35,18 @@ module "oregon" {
   min_size = 1
 }
 
+module "California" {
+  source = "./module"
+
+  region = "us-west-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
 module "Ohio" {
   source = "./module"
   region = "us-east-2"
@@ -50,6 +62,103 @@ module "Ohio" {
 module "ireland" {
   source = "./module"
   region = "eu-west-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Africa" {
+  source = "./module"
+  region = "af-south-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+
+module "Hong Kong" {
+  source = "./module"
+  region = "ap-east-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "mumbai" {
+  source = "./module"
+  region = "ap-south-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Seoul" {
+  source = "./module"
+  region = "ap-northeast-2"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Singapur" {
+  source = "./module"
+  region = "ap-southeast-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Sydney" {
+  source = "./module"
+  region = "ap-southeast-2"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Tokyo" {
+  source = "./module"
+  region = "ap-northeast-1"
+  image_owner = "137112412989"
+
+  desired_capacity = 1
+
+  max_size = 1
+
+  min_size = 1
+}
+
+module "Canada" {
+  source = "./module"
+  region = "ca-central-1"
   image_owner = "137112412989"
 
   desired_capacity = 1
